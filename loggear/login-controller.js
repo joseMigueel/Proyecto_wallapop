e5a96edf9138eb8a4a84b301f3ccce7167af25ce
joseMigueel <josemiguemarre99@gmail.com>
2024-11-27 12:16:56 +0000

    update de login_controller

---
+++ loggear/login-controller.js
@@ -0,0 +1,12 @@
+
+
+export function loginController(loginForm) {
+
+    loginForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const userNameElement = loginForm.querySelector("#name-user")
+        const passwordElement = loginForm.querySelector("#password")
+    })
+
+}
---
+++ login.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const loginForm = document.querySelector("form")
+}).
