efc8a06786903b5c5021ddd918a391b041b85dab
joseMigueel <josemiguemarre99@gmail.com>
2024-11-27 12:22:30 +0000

    update

--- loggear/login-controller.js
+++ loggear/login-controller.js
@@ -1,3 +1,4 @@
+import { REGEXP } from "../utilidades/constants";
 
 
 export function loginController(loginForm) {
@@ -7,6 +8,24 @@ export function loginController(loginForm) {
 
         const userNameElement = loginForm.querySelector("#name-user")
         const passwordElement = loginForm.querySelector("#password")
+
+        const userName = userNameElement.value;
+        const password = passwordElement.value;
+
+
+        const userRegExp = new RegExp(REGEXP.user);
+        if (!userRegExp.test(userName)){
+            alert('El formato que utiliza esta incorrecto')
+        } else {
+            handleLoginUser(userName, password) 
+        }
     })
 
+}
+
+async function handleLoginUser(userName, password) {
+    const token = await loginUser(userName, password);
+
+    localStorage.setItem("jwt", token);
+    window.location.href = "/"
 }
