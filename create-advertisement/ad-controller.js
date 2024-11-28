735cd34fcf54ec0f8e85e55e4157510c407c7725
joseMigueel <josemiguemarre99@gmail.com>
2024-11-28 13:26:06 +0000

    update ad-controller

--- create-advertisement/ad-controller.js
+++ create-advertisement/ad-controller.js
@@ -4,6 +4,27 @@ export function createAdsController(createAdsForm){
     createAdsForm.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        const productNameElement = createAdsForm.querySelector("#product-name")
+        const productNameElement = createAdsForm.querySelector("#product-name");
+        const adsDescriptionElement = createAdsForm.querySelector("#ads-content");
+        const productImageElement = createAdsForm.querySelector("#ads-image");
+        const productPriceElement = createAdsForm.querySelector("#ads-price");
+        const productTypeElement = createAdsForm.querySelector("#ads-type");
+
+        const productName = productNameElement.value;
+        const adsDescription = adsDescriptionElement.value;
+        const productImage = productImageElement.value;
+        const productPrice = productPriceElement.value;
+        const productType = productTypeElement.value;
+
+        manageAdsCreation(productName,adsDescription, productImage, productPrice, productType )
     })
+
+    async function manageAdsCreation(productName,adsDescription, productImage, productPrice, productType) {
+        try {
+            await createAds(productName,adsDescription, productImage, productPrice, productType)
+            window.location.href = "/"
+        } catch (error) {
+            alert(error.message)
+        }
+    }
 }
